a6717f26c0fb350e487c9dba377d4953e60f245b
LOGESHJ45 <117885867+LOGESHJ45@users.noreply.github.com>
2025-03-19 13:27:25 +0530

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -35,7 +35,7 @@ def call(Map config) {
 
         stage('Push to Docker Hub') {
             script {
-                docker.withRegistry('https://novacisdockerhub.hub.docker.com',docker-hub-cred') {
+                docker.withRegistry('https://novacisdockerhub.hub.docker.com','docker-hub-cred') {
                     docker.image(imageWithTag).push()
                 }
             }
